d0d4b1d9f236cc8126be86dc063bb0eda61532b6
Shivam1504 <shivamji019@gmail.com>
2024-05-15 15:35:35 +0530

    update

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/App_Context';
+import { AppContext } from '../context/App_context';
 import { ToastContainer, toast,Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {useNavigate} from 'react-router-dom'
